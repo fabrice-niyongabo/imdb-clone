4a7616fa17f7478fdcfad72f9b0afdcff4985b08
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-11-25 11:29:02 +0200

    configured env variables

---
+++ src/constants/index.ts
@@ -0,0 +1,1 @@
+export const API_TOKEN = import.meta.env.VITE_APP_API_TOKEN;
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <main>home view {{ API_TOKEN }}</main>
+</template>
+
+<script setup lang="ts">
+import { API_TOKEN } from "../../constants";
+</script>
