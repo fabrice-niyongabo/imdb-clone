ad54b8baf690cfcfa7cf90b85ab902fcdfc32cea
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-12-01 15:01:49 +0200

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
 <template>
-  <!-- <header>
-    <div class="wrapper"> 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header> -->
-
-
   <Header />
-
   <RouterView />
+  <Footer />
 </template>
 
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router' 
-import Header from './components/Header/Header.vue'
+import { RouterLink, RouterView } from "vue-router";
+import Header from "./components/Header/index.vue";
+import Footer from "./components/Footer/index.vue";
 </script>
-
-<style scoped>
- 
-</style>
---
+++ src/components/Footer/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer class="bg-black pb-5">
+    <div class="container mx-auto">
+      <div class="flex items-center justify-center">
+        <v-btn class="!bg-imdb-gold !normal-case" :elevation="0"
+          >Sign in for more access</v-btn
+        >
+      </div>
+      <div class="mt-10 flex items-center justify-center">
+        <ul class="m-0 p-0 flex gap-5 md:gap-10">
+          <li>
+            <a href="#"><v-icon icon="mdi-instagram" /></a>
+          </li>
+          <li>
+            <a href="#"><v-icon icon="mdi-twitter" /></a>
+          </li>
+          <li>
+            <a href="#"><v-icon icon="mdi-youtube" /></a>
+          </li>
+          <li>
+            <a href="#"><v-icon icon="mdi-facebook" /></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <p class="text-center pt-5 text-sm text-gray-300">
+      &copy;2023 <a href="mailto:ninga.fab@gmail.com">ninga.fab@gmail.com</a>
+    </p>
+  </footer>
+</template>
+<script lang="ts">
+export default {};
+</script>
