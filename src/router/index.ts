764ce49a9b923fc50b9929b4ce9d61dacffd5e1a
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-12-01 20:21:23 +0200

    basic routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/Home/index.vue";
 import MovieDetails from "../views/MovieDetails/index.vue";
 import TVDetails from "../views/TVDetails/index.vue";
+import Find from "../views/Find/index.vue";
+import Login from "../views/Login/index.vue";
+import Register from "../views/Register/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +23,21 @@ const router = createRouter({
       path: "/tv/:id",
       name: "tvDetails",
       component: TVDetails,
+    },
+    {
+      path: "/find",
+      name: "find",
+      component: Find,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register,
     },
   ],
 });
