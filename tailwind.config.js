5c78e3ac6b585c671f2fdd08051f4ed078f690a6
Fabrice Niyongabo <ninga.fab@gmail.com>
2024-05-09 11:17:41 +0200

    fix tailwind warnings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 
-const colors = require("tailwindcss/colors");
-
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      ...colors,
-      "imdb-gold": "#F5C518",
-      "imdb-blue": "#5799ef",
-      "imdb-black": "#121212",
-      "imdb-light-black": "#252525",
-      "imdb-light-blue-bg": "#313131",
-      "imdb-search-bg": "#222222",
-      "imdb-gray-text": "#6d6d6d",
+    extend: {
+      colors: {
+        "imdb-gold": "#F5C518",
+        "imdb-blue": "#5799ef",
+        "imdb-black": "#121212",
+        "imdb-light-black": "#252525",
+        "imdb-light-blue-bg": "#313131",
+        "imdb-search-bg": "#222222",
+        "imdb-gray-text": "#6d6d6d",
+      },
     },
   },
   plugins: [],
